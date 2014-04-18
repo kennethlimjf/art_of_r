661cf7e215d87d7144b7b31f3cd5b04426b32aee
Kenneth Lim <kennethlimjf@gmail.com>
2014-04-18 16:32:09 +0800

    Started on Chapter 3

---
+++ chapter_3.r
@@ -0,0 +1,10 @@
+# --------------------
+# Chapter 3: Matrices
+# --------------------
+
+# Matrices are vectors with columns and rows.
+# However, vector is not a matrix.
+
+# Creating a matrix
+x <- matrix(c(1,2,3,4,5,6), nrow=3, ncol=2)
+x
